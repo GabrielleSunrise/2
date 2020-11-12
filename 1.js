de7ffc15014be59372188d5e31b9964f6009414e
GabrielleSunrise <45597740+GabrielleSunrise@users.noreply.github.com>
2020-11-12 22:16:13 +0500

    Update 1.js

--- 1.js
+++ 1.js
@@ -1,8 +1,6 @@
 document.getElementById("hamburger").onclick = function() {open()};
 
 function open() {
-    document.getElementById("stripe1").classList.toggle("isactive");
-    document.getElementById("stripe2").classList.toggle("isactive");
-    document.getElementById("stripe3").classList.toggle("isactive");
+    document.getElementById("hamburger").classList.toggle("isactive");
     document.getElementById("mobile-menu").classList.toggle("show");
 }
